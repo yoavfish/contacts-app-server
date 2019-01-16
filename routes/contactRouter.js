c7fded2ddc8ff034f63e41008c1cd562316159f8
Yoav Fishman <fish@wearegofer.com>
2019-01-16 19:14:23 +0200

    Adding Search to the api

--- routes/contactRouter.js
+++ routes/contactRouter.js
@@ -1,17 +1,30 @@
 const express = require('express')
 const Contact = require('../models/contacts')
 
-const contactRouter = express.Router();
+const contactRouter = express.Router()
 
 contactRouter
     .get('/', (req,res) => {
-        Contact.find({}, (err, data) => {
-            if (err){
-                return res.status(400).json(err)
-            }
-            res.status(200).json(data)
+        const searchText = req.query.searchText
+        let queryObj = {}
+
+        if(searchText) {
+            const searchObj = {$regex: searchText, $options: 'i'}
+            queryObj = {$or:[
+                {'name.first': searchObj},
+                {'name.last': searchObj},
+                {'location.street': searchObj},
+                {'cell': searchObj}
+            ]}
+        }
+
+        Contact.find(queryObj, (err, data) => {
+           if (err){
+               return res.status(400).json(err)
+           }
+           res.status(200).json(data)
         })
-    }) 
+    })
 
 contactRouter
     .post('/', (req, res) => {
