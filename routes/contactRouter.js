c83e565f466b4de1838af93f5978c94d464fd8fe
Yoav Fishman <fish@wearegofer.com>
2019-01-16 16:46:07 +0200

    adding delete to api

--- routes/contactRouter.js
+++ routes/contactRouter.js
@@ -5,22 +5,32 @@ const contactRouter = express.Router();
 
 contactRouter
     .get('/', (req,res) => {
-        Contact.find({}, (err, contacts) => {
+        Contact.find({}, (err, data) => {
             if (err){
                 return res.status(400).json(err)
             }
-            res.status(200).json(contacts)
+            res.status(200).json(data)
         })
     }) 
 
 contactRouter
-    .post('/', (req, res) =>{
+    .post('/', (req, res) => {
         let contact = new Contact(req.body)
         contact.save((err, contact) => {
             if (err){
                 return res.status(400).json(err)
             }
             res.status(200).json(contact)
+        })
+    })
+
+contactRouter 
+    .delete('/:id', (req, res) => {
+        Contact.findByIdAndDelete(req.params.id, (err, data) => {
+            if (err){
+                return res.status(400).json(err)
+            }
+            res.status(204).json(data)
         })
     })
 
