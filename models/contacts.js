2bf000499c0a722b651491694b835cf224360c1b
Yoav Fishman <fish@wearegofer.com>
2019-01-17 16:19:07 +0200

    normalizing strings before saving to db

--- models/contacts.js
+++ models/contacts.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose')
 var mongoosePaginate = require('mongoose-paginate')
 
+toLower = string => string.toLowerCase()
+
 const nameSchema = new mongoose.Schema({
-  title: String,
-  first: String,
-  last: String
+  title: { type: String, set: toLower } ,
+  first: { type: String, set: toLower } ,
+  last: { type: String, set: toLower } 
 })
 
 const locationSchema = new mongoose.Schema({
-  street: String,
-  city: String,
-  state: String,
-  postcode: String,
+  street: { type: String, set: toLower } ,
+  city: { type: String, set: toLower } ,
+  state: { type: String, set: toLower } ,
+  postcode: { type: String, set: toLower } ,
   coordinates:{
     latitude: Number,
     longitude: Number
@@ -19,15 +21,15 @@ const locationSchema = new mongoose.Schema({
 })
 
 const pictureSchema = new mongoose.Schema({
-  large: String,
-  medium: String,
-  thumbnail: String
+  large: { type: String, set: toLower } ,
+  medium: { type: String, set: toLower } ,
+  thumbnail: { type: String, set: toLower } 
 })
 
 const contactSchema = new mongoose.Schema({
   name: [nameSchema],
   location: [locationSchema],
-  cell: String,
+  cell: { type: String, set: toLower } ,
   picture: [pictureSchema],
   createdOn: { type: Date, default: Date.now}
 })
