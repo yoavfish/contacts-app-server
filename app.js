10f1b2ad4509ce581a1f2d8b1e6eca5bd2a8e393
Yoav Fishman <fish@wearegofer.com>
2019-01-19 17:54:43 +0200

    adding controller and cleaning app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const {contactRouter} = require('./routes/contactRouter')
+const { contactRouter } = require('./routes/contactRouter')
 const express = require('express')
-const path = require('path')
 const chalk = require('chalk')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
@@ -11,11 +10,6 @@ const port = process.env.PORT || 3000
 
 app.use(morgan('tiny'))
 app.use(bodyParser.json())
-
-app.get('/', (req, res) => {
-  console.log('we have a get request')
-})
-
 app.use('/api/contacts', contactRouter)
 
 mongoose.connect('mongodb://contactsdbuser:OGdG9gmSNXNcBP8U@cluster0-shard-00-00-jfcg6.mongodb.net:27017/contacts?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', {useNewUrlParse: true})
@@ -24,7 +18,7 @@ const db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {
-  console.log('Connected to mongoDB')
+  console.log(`Connected to ${chalk.green('mongoDB')}`)
 
   app.listen(port, () => {
     console.log(`API Server Listening on port ${chalk.green(port)}`)
--- routes/contactRouter.js
+++ routes/contactRouter.js
@@ -1,68 +1,17 @@
 const express = require('express')
-const Contact = require('../models/contacts')
-
+const ContactsController = require('../controllers/contacts')
 const contactRouter = express.Router()
 
 contactRouter
-    .get('/', (req,res) => {
-        const searchText = req.query.searchText
-        const sort = req.query.sortType && req.query.sortDirection ? {[req.query.sortType] : req.query.sortDirection} : {'name.first': 1}
-        const paginationOptions = {
-            sort,
-            offset: (req.query.page) * 100, 
-            limit: 100
-          };
-
-        let queryObj = {}
-
-        if(searchText) {
-            const searchObj = {$regex: searchText, $options: 'i'}
-            queryObj = {$or:[
-                {'name.first': searchObj},
-                {'name.last': searchObj},
-                {'location.street': searchObj},
-                {'cell': searchObj}
-            ]}
-        }
-
-        Contact.paginate(queryObj, paginationOptions).then((data, err) => {
-            if (err){
-                return res.status(400).json(err)
-            }
-            res.status(200).json(data)
-         })
-    })
+    .get('/', ContactsController.get_contacts)
 
 contactRouter
-    .post('/', (req, res) => {
-        const contact = new Contact(req.body)
-        contact.save((err, contact) => {
-            if (err){
-                return res.status(400).json(err)
-            }
-            res.status(200).json(contact)
-        })
-    })
+    .post('/', ContactsController.create_contact)
 
 contactRouter 
-    .put('/:id', (req, res) => {
-        const contact = new Contact(req.body)
-        Contact.findByIdAndUpdate(req.params.id, contact, (err, data) => {
-            if (err){
-                return res.status(400).json(err)
-            }
-            res.status(200).json(data)
-        })
-    })
+    .put('/:id', ContactsController.update_contact)
 
 contactRouter 
-    .delete('/:id', (req, res) => {
-        Contact.findByIdAndDelete(req.params.id, (err, data) => {
-            if (err){
-                return res.status(400).json(err)
-            }
-            res.status(200).json(data)
-        })
-    })
+    .delete('/:id', ContactsController.delete_contact)
 
 module.exports.contactRouter = contactRouter
